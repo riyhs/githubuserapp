ec48a3f64ae57837d6cda6fd00e7b7628afd7f85
Your Name <you@example.com>
2020-06-24 08:17:07 +0700

    edit DetailViewModel.kt and change style SearchView

--- app/src/main/java/com/riyaldi/githubuserapp/model/DetailViewModel.kt
+++ app/src/main/java/com/riyaldi/githubuserapp/model/DetailViewModel.kt
@@ -1,2 +1,109 @@
 package com.riyaldi.githubuserapp.model
 
+import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.riyaldi.githubuserapp.data.User
+import cz.msebera.android.httpclient.Header
+import org.json.JSONArray
+import org.json.JSONObject
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class DetailViewModel : ViewModel() {
+    val liveDataUser = MutableLiveData<ArrayList<User>>()
+    val list = ArrayList<User>()
+
+    fun getUserData(): MutableLiveData<ArrayList<User>> {
+        return liveDataUser
+    }
+
+    fun setUserData(login: String, context: Context, method: String) {
+        val urlSearch = "https://api.github.com/users/$login/$method"
+        val client = AsyncHttpClient()
+        val mToken = "6d75636beac96e82445c4ae9e66a1084a7bd38ca"
+
+        client.addHeader("Authorization", "token $mToken")
+        client.addHeader("User-Agent", "request")
+
+        client.get(urlSearch, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
+
+                val result = responseBody?.let { String(it) }
+                try {
+                    val responseArray = JSONArray(result)
+
+                    for (i in 0 until responseArray.length()) {
+                        val item = responseArray.getJSONObject(i)
+                        val username = item.getString("login")
+                        getDetailUserData(username, context)
+                    }
+                    liveDataUser.postValue(list)
+                } catch (e: Exception) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun getDetailUserData(username: String, context: Context) {
+        val urlDetailUser = "https://api.github.com/users/$username"
+        val client = AsyncHttpClient()
+        val mToken = "6d75636beac96e82445c4ae9e66a1084a7bd38ca"
+
+        client.addHeader("Authorization", "token $mToken")
+        client.addHeader("User-Agent", "request")
+
+        client.get(urlDetailUser, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
+                val result = responseBody?.let { String(it) }
+                try {
+                    val responseObject = JSONObject(result)
+
+                    val user = User(
+                        name = responseObject.getString("name"),
+                        username = username,
+                        company = responseObject.getString("company"),
+                        location = responseObject.getString("location"),
+                        bio = responseObject.getString("bio"),
+                        followers = responseObject.getString("followers"),
+                        following = responseObject.getString("following"),
+                        followersUrl = responseObject.getString("followers_url"),
+                        followingUrl = responseObject.getString("following_url"),
+                        repositories = responseObject.getString("public_repos"),
+                        photoUrl = responseObject.getString("avatar_url")
+                    )
+                    list.add(user)
+                    liveDataUser.postValue(list)
+                } catch (e: Exception) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
--- app/src/main/java/com/riyaldi/githubuserapp/ui/FollowersFragment.kt
+++ app/src/main/java/com/riyaldi/githubuserapp/ui/FollowersFragment.kt
@@ -4,27 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.loopj.android.http.AsyncHttpClient
-import com.loopj.android.http.AsyncHttpResponseHandler
 import com.riyaldi.githubuserapp.R
 import com.riyaldi.githubuserapp.adapter.UserRecyclerViewAdapter
 import com.riyaldi.githubuserapp.data.User
-import cz.msebera.android.httpclient.Header
+import com.riyaldi.githubuserapp.model.DetailViewModel
 import kotlinx.android.synthetic.main.fragment_followers.*
-import org.json.JSONArray
-import org.json.JSONObject
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class FollowersFragment : Fragment() {
 
-    private val listFollowers = arrayListOf<User>()
+    private var listFollowers = arrayListOf<User>()
 
-    // LoopJ
-    private val client = AsyncHttpClient()
-    private val mToken = "6d75636beac96e82445c4ae9e66a1084a7bd38ca"
+    private lateinit var viewModel: DetailViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_followers, container, false)
@@ -32,12 +27,32 @@ class FollowersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        showLoading(false)
 
         val activity: DetailActivity = activity as DetailActivity
         val user: User = activity.getMyData()
 
-        getSimpleUserData(user.username)
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(DetailViewModel::class.java)
+
+        showData(user.username)
         showRecyclerView()
+    }
+
+    private fun showData(username: String) {
+        showLoading(true)
+        listFollowers.clear()
+        viewModel.setUserData(username, requireContext(), "followers")
+        setViewModel()
+    }
+
+    private fun setViewModel() {
+        viewModel.getUserData().observe(this, Observer { liveUserData ->
+            if (liveUserData != null) {
+                listFollowers = liveUserData
+                rvFragmentFollowers.adapter = UserRecyclerViewAdapter(listFollowers)
+                showLoading(false)
+            }
+        })
     }
 
     private fun showRecyclerView() {
@@ -47,112 +62,11 @@ class FollowersFragment : Fragment() {
         }
     }
 
-    private fun getSimpleUserData(login: String) {
-        pbFollowers.visibility = View.VISIBLE
-
-        val urlSearch = "https://api.github.com/users/$login/followers"
-
-        client.addHeader("Authorization", "token $mToken")
-        client.addHeader("User-Agent", "request")
-
-        client.get(urlSearch, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
-                pbFollowers.visibility = View.INVISIBLE
-
-                val result = responseBody?.let { String(it) }
-
-                try {
-                    val responseArray = JSONArray(result)
-
-                    for (i in 0 until responseArray.length()) {
-                        val item = responseArray.getJSONObject(i)
-                        val username = item.getString("login")
-
-                        getDetailUserData(username)
-                    }
-
-                    rvFragmentFollowers.adapter = UserRecyclerViewAdapter(listFollowers)
-                } catch (e: Exception) {
-                    Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
-                    e.printStackTrace()
-                }
-            }
-
-            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
-                pbFollowers.visibility = View.INVISIBLE
-
-                val errorMessage = when (statusCode) {
-                    401 -> "$statusCode : Bad Request"
-                    403 -> "$statusCode : Forbidden"
-                    404 -> "$statusCode : Not Found"
-                    else -> "$statusCode : ${error?.message}"
-                }
-                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
-            }
-
-        })
-    }
-
-    private fun getDetailUserData(username: String) {
-        pbFollowers.visibility = View.VISIBLE
-        val urlDetailUser = "https://api.github.com/users/$username"
-
-        client.addHeader("Authorization", "token $mToken")
-        client.addHeader("User-Agent", "request")
-
-        client.get(urlDetailUser, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
-                pbFollowers.visibility = View.INVISIBLE
-
-                val result = responseBody?.let { String(it) }
-
-                try {
-                    val responseObject = JSONObject(result)
-
-                    val name = responseObject.getString("name")
-                    val company = responseObject.getString("company")
-                    val location = responseObject.getString("location")
-                    val bio = responseObject.getString("bio")
-                    val followers = responseObject.getString("followers")
-                    val following = responseObject.getString("following")
-                    val followersUrl = responseObject.getString("followers_url")
-                    val followingUrl = responseObject.getString("following_url")
-                    val repositories = responseObject.getString("public_repos")
-                    val photoUrl = responseObject.getString("avatar_url")
-
-                    val user = User(
-                        name = name,
-                        username = username,
-                        company = company,
-                        location = location,
-                        bio = bio,
-                        followers = followers,
-                        following = following,
-                        followersUrl = followersUrl,
-                        followingUrl = followingUrl,
-                        repositories = repositories,
-                        photoUrl = photoUrl
-                    )
-                    listFollowers.add(user)
-                    rvFragmentFollowers.adapter = UserRecyclerViewAdapter(listFollowers)
-                } catch (e: Exception) {
-                    Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
-                    e.printStackTrace()
-                }
-            }
-
-            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
-                pbFollowers.visibility = View.INVISIBLE
-
-                val errorMessage = when (statusCode) {
-                    401 -> "$statusCode : Bad Request"
-                    403 -> "$statusCode : Forbidden"
-                    404 -> "$statusCode : Not Found"
-                    else -> "$statusCode : ${error?.message}"
-                }
-                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
-            }
-
-        })
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            pbFollowers.visibility = View.VISIBLE
+        } else {
+            pbFollowers.visibility = View.GONE
+        }
     }
 }
